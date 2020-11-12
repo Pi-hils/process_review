f354e4d4174304cb5d3ba5b89f1d3b04d0179075
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-12 14:13:52 +0000

    given no high or low freq, return original soundwave

--- spec/Filters_spec.rb
+++ spec/Filters_spec.rb
@@ -2,7 +2,11 @@ require 'Filter'
 
 describe Filter do
   #[40,50,60,1000], 40,1000 |  [40,50,60,1000] no modification
-  it "returns the soundwave without nno modification of the low or high frequency" do
+  it "returns the soundwave without any modification of the low or high frequency" do
     expect(subject.music_freq([40,50,60,1000], 40,1000)).to eq [40,50,60,1000]
+  end
+
+  it "returns sounndwave without modification if no low or high value given" do
+    expect(subject.music_freq([40,50,55,70,89,100],nil,nil)).to eq [40,50,55,70,89,100]
   end
 end

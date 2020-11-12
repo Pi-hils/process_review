fade7480b1026b673ccca58af03202da7f94df36
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-12 15:19:05 +0000

    always requires 3 arguements

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -5,4 +5,8 @@ describe Filter do
   it "has a soundwave which stays the same if the values are within the range" do
     expect(subject.music_filter([40,50,60,1000], 40,1000)).to eq [40,50,60,1000]
   end
+
+  it "doesn't mater the amount of integers within the array" do
+    expect(subject.music_filter([90,40],60,100)).to eq [90,40] 
+  end
 end

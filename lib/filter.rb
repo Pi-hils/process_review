cf3bc73a31efaa4782ff99319f0daa5ab8633315
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-12 15:30:47 +0000

    highest value of freq

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,5 +1,14 @@
 class Filter
   def music_filter(sound_wave,lowest_filter, highest_filter)
-    sound_wave
+    new_sound_wave = []
+
+    sound_wave.each do |freq|
+      if freq > highest_filter
+        new_sound_wave << highest_filter
+      else
+        new_sound_wave<<freq
+      end
+    end
+    return new_sound_wave
   end
 end
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -9,4 +9,9 @@ describe Filter do
   it "doesn't mater the amount of integers within the array" do
     expect(subject.music_filter([90,40],60,100)).to eq [90,40] 
   end
+
+  #[20, 40, 60, 90, 1050], 40, 1000| [40,40,60,90, 1000]     #modified the high freq  
+  it "converts to the default high freq if any value to about the highest_filter" do
+    expect(subject.music_filter([40, 40, 60, 90, 1050], 40, 1000)).to eq [40,40,60,90, 1000]
+  end
 end

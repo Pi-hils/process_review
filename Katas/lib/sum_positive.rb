02720bdf84be4a72507ec979c58d9d30492bf3dd
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-13 14:15:43 +0000

    add only the positive numbers

--- Katas/lib/sum_positive.rb
+++ Katas/lib/sum_positive.rb
@@ -2,8 +2,10 @@ class Addition
   def positive_sum(arr)
     pos_only = 0
     arr.each do |pos_num|
+      if pos_num.positive?
       pos_only += pos_num
     end
+  end
    return pos_only
   end
 end
--- Katas/spec/sum_positive_spec.rb
+++ Katas/spec/sum_positive_spec.rb
@@ -6,8 +6,12 @@ describe Addition do
     expect(subject.positive_sum([1,4,7,12])).to eq (24)
   end
 
-  #1) Ensure that the number is positve
+  #1) Ensure to add any digit together
   it 'will ensure the numbers are positive' do
     expect(subject.positive_sum([1,2,3])).to eq 6
+  end
+
+  it 'iterates through an array and ensure only positives are added' do
+    expect(subject.positive_sum([1,2,5,-3])).to eq 8
   end
 end

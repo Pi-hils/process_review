618cb545a5149c3c568647d95e5199d9943be3de
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-16 11:27:19 +0000

    incorrect words have ~ before and after the word

--- review_3/lib/spell_checker.rb
+++ review_3/lib/spell_checker.rb
@@ -1,5 +1,11 @@
 class SpellCheck
   def spell(str)
     "Hello"
+
+    if str == "Hello"
+      return "Hello"
+    elsif str != "Hello"
+      return "~#{str}~"
+    end
   end
  end
--- review_3/spec/spell_checker_spec.rb
+++ review_3/spec/spell_checker_spec.rb
@@ -1,8 +1,14 @@
 require 'spell_checker'
 
 describe SpellCheck do
+  subject(:spellCheck) {described_class.new}
   #"Hello" | "Hello" 
   it "checks that a spelling is a string" do
-    expect(subject.spell("Hello")).to be_kind_of String
+    expect(spellCheck.spell("Hello")).to be_kind_of String
+  end
+
+  #"WRDS" | "~WRDS~"|
+  it 'will output a word with ~ if the word is incorrectly spelt' do
+    expect(spellCheck.spell("WRDS")).to eq ("~WRDS~")
   end
 end

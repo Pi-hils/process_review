3dc86426fe22788196ce59185fc00ad294bd02b7
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-25 16:52:57 +0000

    decreases and increases

---
+++ Katas/Fake_Binary/fake_binary.rb
@@ -0,0 +1,14 @@
+# str.split('').map{ |x| x.to_i < 5 ? 0 : 1 }.join
+class Faking
+def fake_bin(str)
+  digits = str.split("").map{ |j| 
+    if j.to_i < 5 
+    0
+    elsif j.to_i > 5
+    1
+    end
+  }
+
+end
+end
+

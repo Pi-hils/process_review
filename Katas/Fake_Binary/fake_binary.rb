f99d227bd277a07029bb0ee44168688e802584f1
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-25 17:28:07 +0000

    Refactor

--- Katas/Fake_Binary/fake_binary.rb
+++ Katas/Fake_Binary/fake_binary.rb
@@ -1,14 +1,7 @@
 # str.split('').map{ |x| x.to_i < 5 ? 0 : 1 }.join
 class Faking
-def fake_bin(str)
-  digits = str.split("").map{ |j| 
-    if j.to_i < 5 
-    0
-    elsif j.to_i >= 5
-    1
-    end
-  }
-p digits.join(" ")
-end
+  def fake_bin(str)
+    str.split("").map{ |j|  j.to_i < 5 ? 0 : 1}.join(" ")
+  end
 end
 

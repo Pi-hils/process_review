c33c79b4d6e9ffa6f12276175b7de5a9251b83f5
Hilda Amponsah <hilda.amp@hotmail.com>
2020-11-26 11:18:47 +0000

    leetcode

--- leetcode/lib/robot.rb
+++ leetcode/lib/robot.rb
@@ -1,8 +1,15 @@
 class Robot
   def movement(str)
+   moves = { [:l => -1], [:r => +1], [:u => 1], [:d => -1] }
+
     if str == "UD" || str == "LR"
       return true
     end
-    
+
+    if str > 0
+      return true
+    else
+      false
+    end
   end
 end

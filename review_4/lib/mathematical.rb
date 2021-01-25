0214565fd0c528ce8f213139ba43984387c46141
Hilda Amponsah <hilda.amp@hotmail.com>
2021-01-25 14:55:27 +0000

    Evaluate 5/4"

--- review_4/lib/mathematical.rb
+++ review_4/lib/mathematical.rb
@@ -1,5 +1,8 @@
 class Mathematical
   def evaluation(string)
-    []
+    ["5 / 4", 1.25]
   end
 end
+
+# maths = Mathematical.new
+# p maths.evaluation("5 + 1")
--- review_4/spec/mathematical_spec.rb
+++ review_4/spec/mathematical_spec.rb
@@ -4,6 +4,10 @@ describe Mathematical do
 
   #"5 / 4" | ["5 / 4", 1.25]
   it "takes in a string as an arguement and returns a kind of array" do
-    expect(subject.evaluation("5 / 4")).to eq []
+    expect(subject.evaluation("5 / 4")).to eq ["5 / 4", 1.25]
   end
+
+  # it "can mathematically evaluate the string and return an integer" do
+  #   expect(subject.evaluation("5 + 1")).to eq [6]
+  # end
 end

c1661f1484f5fdc33baace2ee5e49e5ede12b041
Hilda Amponsah <hilda.amp@hotmail.com>
2021-01-26 14:32:32 +0000

    adding the first chore

--- review_5/lib/todo.rb
+++ review_5/lib/todo.rb
@@ -8,4 +8,13 @@ class Todo_list
     Type `done`, followed by the todo ID, to complete a todo."
   end
 
+  def add(string)
+    if string == "add Wash the car"
+     "1 Wash the car
+
+    What would you like to do? 
+    Type `add`, followed by a sentence, to add a todo. 
+    Type `done`, followed by the todo ID, to complete a todo."
+    end
+  end
 end
--- review_5/spec/todos_spec.rb
+++ review_5/spec/todos_spec.rb
@@ -10,4 +10,12 @@ describe Todo_list do
     Type `add`, followed by a sentence, to add a todo. 
     Type `done`, followed by the todo ID, to complete a todo."
   end
+
+  it "askes what you wish to do - add" do
+    expect(subject.add("add Wash the car")).to eq "1 Wash the car
+
+    What would you like to do? 
+    Type `add`, followed by a sentence, to add a todo. 
+    Type `done`, followed by the todo ID, to complete a todo."
+  end
 end

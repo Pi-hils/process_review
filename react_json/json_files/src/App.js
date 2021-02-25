02630a4115b44ccd0bc5dca8c8971790d2746ef9
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-25 22:44:55 +0000

    downloaded react files,added json file with data.displaying json Files in DB for react

---
+++ react_json/json_files/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='response'>
+    <h1>Hello, World</h1>
+    <p>Displaying records from a json file using MAP() method in React</p>
+    </div>
+  );
+}
+
+export default App;

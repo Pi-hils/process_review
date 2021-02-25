216d616570a422e03c376a0926ff8f25cdcddd6e
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-25 23:27:33 +0000

    json being rendered in boxes on port 3000

--- react_json/json_files/src/App.js
+++ react_json/json_files/src/App.js
@@ -1,12 +1,22 @@
-
-import React from 'react';
+import React from 'react'
 import './App.css';
+import Database from './database.json'
 
 function App() {
   return (
     <div className='response'>
-    <h1>Hello, World</h1>
-    <p>Displaying records from a json file using MAP() method in React</p>
+        <h1>Hello, World</h1>
+        <p>Displaying records from a json file using MAP() method in React</p>
+      <div className="info">
+        { Database.map(details => {
+          return(
+          <div key={ details.id } className="details">
+            <h3>{ details.name  }</h3>
+            <p>{ details.email }</p>
+        </div>
+          )
+        })}
+    </div>
     </div>
   );
 }
